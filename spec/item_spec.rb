9d97ec6a25375510aa60703826b0fcde5a6b8a01
Sam Tran <samuelttran@gmail.com>
2023-09-26 09:35:01 -0600

    Write test for #current_high_bid

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -38,4 +38,14 @@ RSpec.describe Item do
       expect(@item1.bids).to eq({@attendee2 => 20, @attendee1 => 22})
     end
   end
+
+  describe '#current_high_bid' do
+    it 'returns highest bid for item' do
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      expect(@item1.bids).to eq({@attendee2 => 20, @attendee1 => 22})
+
+      expect(@item1.current_high_bid).to eq(22)
+    end
+  end
 end
