aba298a10faf995907e99640a06840cc712fa6e3
Sam Tran <samuelttran@gmail.com>
2023-09-26 09:32:22 -0600

    Add #add_bid, add @bid to attr_reader

--- lib/item.rb
+++ lib/item.rb
@@ -1,7 +1,11 @@
 class Item
-  attr_reader :name
+  attr_reader :name, :bids
   def initialize(item_name)
     @name = item_name
     @bids = {}
+  end
+
+  def add_bid(attendee, bid_amount)
+    @bids[attendee] = bid_amount
   end
 end
