633a08ed690b4552dd9c0fde63358b1c4d264114
Sam Tran <samuelttran@gmail.com>
2023-09-26 10:15:55 -0600

    Add helper each_item_highest_bid

--- lib/auction.rb
+++ lib/auction.rb
@@ -14,4 +14,18 @@ class Auction
       no_bids
     end
   end
+
+  def each_item_highest_bid
+    highest_bids = Hash.new
+    @items.each do |item|
+      all_bids = item.bids
+      max_bidder = all_bids.max_by { |attendee, amount| amount }
+      highest_bids[item] = max_bidder
+    end
+    item_bids = highest_bids.compact
+    item_bids.reduce({}) do |final, (item, high_bid)|
+      final[item] = high_bid[1]
+      final
+    end
+  end
 end
