0caeae658f32de68fb49357d9a8d3d4802b1dee3
Sam Tran <samuelttran@gmail.com>
2023-09-26 10:23:02 -0600

    Write test for #bidders

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -78,4 +78,21 @@ RSpec.describe Auction do
       expect(@auction.potential_revenue).to eq(87)
     end
   end
+
+  describe '#bidders' do
+    it 'returns array of bidders names' do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+      expect(@auction.bidders).to eq([])
+
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      @item4.add_bid(@attendee3, 50)
+      @item3.add_bid(@attendee2, 15)
+      expect(@auction.bidders).to eq(['Bob', 'Megan', 'Mike'])
+    end
+  end
 end
