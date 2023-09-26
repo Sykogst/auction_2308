46a44b35c30d103bd51168a304344ab5d66f5bc7
Sam Tran <samuelttran@gmail.com>
2023-09-26 09:43:35 -0600

    Write test for #unpopular_items

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe Auction do
   before(:each) do
     @auction = Auction.new
 
+    @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+
     @item1 = Item.new('Chalkware Piggy Bank')
     @item2 = Item.new('Bamboo Picture Frame')
     @item3 = Item.new('Homemade Chocolate Chip Cookies')
@@ -30,7 +34,26 @@ RSpec.describe Auction do
 
       @auction.add_item(@item2)
       expect(@auction.items).to eq([@item1, @item2])
+    end
+  end
+
+  describe '#unpopular_items' do
+    it 'returns list of item OBJ with no bids' do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      @item4.add_bid(@attendee3, 50)
+      expect(@auction.unpopular_items).to eq([@item2, @item3, @item5])
       
+      @item3.add_bid(@attendee2, 15)
+      expect(@auction.unpopular_items).to eq([@item2, @item5])
+
+
     end
   end
 end
